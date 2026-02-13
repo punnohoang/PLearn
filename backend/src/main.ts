b58e16fca7850f99805571d83df6764bff69d0e4
Hoang Phong <hoangphong7889@gmail.com>
2026-02-13 19:27:53 +0700

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: ['http://localhost:3000', 'https://g5-plearn.vercel.app'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 4000);
